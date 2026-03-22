b49be35dbf0068d0b09df053dff70077fdbc2f59
Diego J. Fdez <diego.j.fdez@gmail.com>
2026-03-22 18:34:55 +0100

    Work in progress

---
+++ group-users-add.sh
@@ -0,0 +1,227 @@
+#!/usr/bin/env bash
+
+# Will try to add to community-scripts ORG
+# Author: DJFR
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Usage
+# To use the PVE Group Users Deletion script, run the command below **only** in the Proxmox VE Shell. This script is intended for managing or enhancing the host system directly.
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/tools/pve/create-groups.sh)"
+
+function header_info {
+  clear
+  cat <<"EOF"
+    ____                                          ______                          __  __                       ____       __     __  _           
+   / __ \_________  _  ______ ___  ____  _  __   / ____/________  __  ______     / / / /_______  __________   / __ \___  / /__  / /_(_)___  ____ 
+  / /_/ / ___/ __ \| |/_/ __ `__ \/ __ \| |/_/  / / __/ ___/ __ \/ / / / __ \   / / / / ___/ _ \/ ___/ ___/  / / / / _ \/ / _ \/ __/ / __ \/ __ \
+ / ____/ /  / /_/ />  </ / / / / / /_/ />  <   / /_/ / /  / /_/ / /_/ / /_/ /  / /_/ (__  )  __/ /  (__  )  / /_/ /  __/ /  __/ /_/ / /_/ / / / /
+/_/   /_/   \____/_/|_/_/ /_/ /_/\____/_/|_|   \____/_/   \____/\__,_/ .___/   \____/____/\___/_/  /____/  /_____/\___/_/\___/\__/_/\____/_/ /_/ 
+                                                                    /_/                                                                          
+EOF
+}
+
+spinner() {
+  local pid=$1
+  local delay=0.1
+  local spinstr='|/-\'
+  while ps -p $pid >/dev/null; do
+    printf " [%c]  " "$spinstr"
+    spinstr=${spinstr#?}${spinstr%"${spinstr#?}"}
+    sleep $delay
+    printf "\r"
+  done
+  printf "    \r"
+}
+
+set -eEuo pipefail
+YW=$(echo "\033[33m")
+BL=$(echo "\033[36m")
+RD=$(echo "\033[01;31m")
+GN=$(echo "\033[1;92m")
+CL=$(echo "\033[m")
+TAB="  "
+CM="${TAB}✔️${TAB}${CL}"
+
+# Telemetry
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/api.func) 2>/dev/null || true
+declare -f init_tool_telemetry &>/dev/null && init_tool_telemetry "group-users-add" "pve"
+
+header_info
+echo "Loading..."
+#whiptail --backtitle "Proxmox VE Helper Scripts" --title "Proxmox VE Group Users Addition" --yesno "This will add PVE Users to Groups. Proceed?" 10 58
+
+# C O D I N G    S T A R T S    H E R E   ! ! ! !
+#NODE=$(hostname)
+DOMAIN='institutodh.net'
+
+# CSV file format
+# "Pupil","ID/Passaport","Unit"
+# "Surname1 Surname2, Name1 Name2","ID","GROUP"
+inputFile=${1:-"RegAlum.csv"}
+inputFileTrans="RegAlumT.csv"
+
+# Transliteration: Remove accented characters, ñs and so the like from the CSV file
+iconv -t ASCII//TRANSLIT -f ISO-8859-1  $inputFile > $inputFileTrans
+
+# Extract unit
+unit=$(tail -1 $inputFileTrans | sed  's/\"\,\"/\"\;\"/'g | awk -F';' '{print $3}'| tr -d 'o"\ ')
+echo $unit
+
+# Test if group exists
+groups=$(pveum group list --output-format text --noborder --noheader|awk '{print $1}')
+#groupFound=$(echo $groups | grep -F -w -o "$unit")
+#echo $groupFound
+if [ -z $(echo $groups | grep -F -w -o "$unit") ]; then
+  echo -e "${BL}[Error]${RD} Group $unit not found...${CL}"
+  exit 127 # Soon replace for group creation
+fi
+
+# Extract IDs and Student Names
+students=$(tail +2 RegAlum_transit.csv | sed  's/\"\,\"/\"\;\"/'g | awk -F';' '{print $2";"$1}' | tr -d '\"')
+echo $students
+
+# Loop students to generate various fields for pveum user and pvem pool
+i=0
+while read -r student; do
+  id=$(echo $student | awk -F';' '{print $1}')
+
+  #Get IDs
+  IDs[$i]=$id
+  echo ${IDs[$i]}
+  
+  #Get Student Names
+  aStudentNames[$i]=$(echo -n $student | awk -F';' '{print $2}') 
+  echo ${aStudentNames[$i]}
+  # Generate userid from surnames, first name and ID
+  username=$(echo -n ${aStudentNames[$i]} | tr '[:upper:]' '[:lower:]' \
+                | sed -E 's/^([a-z].*) ([a-z])[a-z]*?, (([a-z])[a-z]*)[ ]?(([a-z])[a-z]*)?/\4\6\1\2/')
+  userids[$i]=$(echo -n "$username$(echo -n ${IDs[$i]} | cut -c6-8)@pve")
+  echo ${userids[$i]}
+
+  # Fullfil Lastnames
+  lastNames[$i]=$(echo -n ${aStudentNames[$i]} | cut -d',' -f1)
+  echo ${lastNames[$i]}
+
+  # Fullfil FirstNames
+  firstNames[$i]=$(echo -n ${aStudentNames[$i]} | cut -d',' -f2 | cut -c2-)
+  echo ${firstNames[$i]}
+
+  # Fullfil emails
+  emails[$i]=$(echo -n "$(echo -n ${userids[$i]} | cut -d'@' -f1)@$DOMAIN")
+  #cut  -d'@' -f1 
+  echo ${emails[$i]}
+
+  # Fullfil User's pool comments
+  poolUserComments[$i]="${aStudentNames[$i]} pool"
+  echo ${poolUserComments[$i]}
+
+  let i=i+1
+done <<<"$students"
+
+
+# Fullfil User comments
+userComments="$unit Student"
+echo $userComments
+# Account Expiration = now() + 10 months (26298000 seconds)
+expiration=$(echo "$(date +"%s")+26298000"|bc)
+echo $expiration
+
+
+# Traverse students parallel arrays to create users, ACLs and pools
+echo -e "${BL}[Info]${GN} Now we wel start adding user to Group $unit...${CL}"
+for j in ${!userids[@]} ;do
+  echo -e "${BL}[Info]${GN} Creating user ${userids[$j]}...${CL}"
+  echo -e "pveum user add ${userids[$j]} \n\
+        --comment \"$userComments\" \n\
+        --email ${emails[$j]} \n\
+        --expire $expiration \n\
+        --firstname \"${firstNames[$j]}\" \n\
+        --lastname \"${lastNames[$j]}\" \n\
+        --groups $unit \n\
+        --password ${IDs[$j]} \n"   
+    echo -e "${BL}[Info]${GN} User ${userids[j]} created.${CL}"
+    #create pool & ACLs
+
+
+    sleep .1
+done
+
+exit
+usersSel=$(echo ${users[$gid]} | tr -s ',' ' ')
+  for userid in $usersSel; do
+    echo -e "${BL}[Info]${GN} Deleting user $userid...${CL}"
+    pveum user delete $userid
+    if [[ "$DELETE_MODE" == "a" ]]; then
+      poolID=$(echo $userid|cut -d'@' -f1)
+      poolFound=$(echo $poolIds | grep -F -w -q "$poolID")
+      if [ -z "$poolFound" ]; then
+        echo -e "${BL}[Info]${GN} Found pool...${CL}"
+        pveum pool delete $poolID
+        echo -e "${BL}[Info]${GN} Pool $userid deleted.${CL}"
+      else
+        echo -e "${BL}[Info]${YW} Pool not found...${CL}"
+      fi
+    fi
+
+
+
+
+
+
+
+
+FORMAT="%-10s %-2s" # %-10s"
+
+i=0
+while read -r group; do
+  groupid=$(echo $group | awk '{print $1}')
+  groups[$i]=$groupid
+  users[$i]=$(echo $group | awk '{print $2}') 
+  someUsers=$(echo ${users[$i]} | cut -c-60 )
+  formatted_line=$(printf "$FORMAT" "$groupid" "$someUsers")
+  menu_items+=("$i" "$formatted_line" "OFF")
+  let i=i+1
+done <<<"$groupUsers"
+
+CHOICES=$(whiptail --title "PVE Group Users Deletion" \
+  --checklist "Select Group(s) to Delete its Users:" 25 100 13 \
+  "${menu_items[@]}" 3>&2 2>&1 1>&3)
+
+if [ -z "$CHOICES" ]; then
+  whiptail --title "PVE Group User Deletion" \
+    --msgbox "No groups selected!" 10 60
+  exit 0
+fi
+
+selected_ids=$(echo "$CHOICES" | tr -d '"' | tr -s ' ' '\n')
+echo "EL $selected_ids"
+
+read -p "Try to delete users pools automatically? (Default: auto) m/a: " DELETE_MODE
+DELETE_MODE=${DELETE_MODE:-a}
+
+
+for gid in $selected_ids; do
+  echo "Grupo $gid"
+  echo ${groups[$gid]}
+  echo ${users[$gid]}
+  usersSel=$(echo ${users[$gid]} | tr -s ',' ' ')
+  for userid in $usersSel; do
+    echo -e "${BL}[Info]${GN} Deleting user $userid...${CL}"
+    pveum user delete $userid
+    if [[ "$DELETE_MODE" == "a" ]]; then
+      poolID=$(echo $userid|cut -d'@' -f1)
+      poolFound=$(echo $poolIds | grep -F -w -q "$poolID")
+      if [ -z "$poolFound" ]; then
+        echo -e "${BL}[Info]${GN} Found pool...${CL}"
+        pveum pool delete $poolID
+        echo -e "${BL}[Info]${GN} Pool $userid deleted.${CL}"
+      else
+        echo -e "${BL}[Info]${YW} Pool not found...${CL}"
+      fi
+    fi
+    echo -e "${BL}[Info]${GN} User $userid deleted.${CL}"
+    sleep .5
+  done
+done
+
+header_info
+echo -e "${GN}Addition process completed.${CL}\n"
